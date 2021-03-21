89d76572f0a835126669403c37d61a3b168b3a35
magdychserg <magdych@mail.ru>
2021-03-21 21:46:15 +1200

    lesson2

--- lesson2.sql
+++ lesson2.sql
@@ -23,4 +23,5 @@ use sample;
 source sample.sql;
 select * from sample;
 -- 4.Ознакомьтесь более подробно с документацией утилиты mysqldump. Создайте дамп единственной таблицы help_keyword базы данных mysql. Причем добейтесь того, чтобы дамп содержал только первые 100 строк таблицы.
+
 mysqldump mysql help_keyword --where="1 limit 100"> mysql.sql
